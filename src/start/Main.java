db367d1d34116b916ad4033fd2420732426baa1c
Simon Hajek <simon.hajek2@etu.univ-lorraine.fr>
2017-09-28 10:39:07 +0200

    Ajout du tag

--- src/start/Main.java
+++ src/start/Main.java
@@ -1,9 +1,5 @@
 package start;
 
-import model.PacmanPainter;
-import engine.GameEngineGraphical;
-import model.PacmanController;
-import model.PacmanGame;
 
 /**
  * lancement du moteur avec le jeu
@@ -12,14 +8,6 @@ public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		// creation du jeu particulier et de son afficheur
-		PacmanGame game = new PacmanGame("helpFilePacman.txt");
-		PacmanPainter painter = new PacmanPainter();
-		PacmanController controller = new PacmanController();
-
-		// classe qui lance le moteur de jeu generique
-		GameEngineGraphical engine = new GameEngineGraphical(game, painter, controller);
-		engine.run();
 	}
 
 }
