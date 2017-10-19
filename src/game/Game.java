903697a96f2a0f968bfb2b55f66e60661277be34
Simon HAJEK <simon@sup.sarl>
2017-10-19 11:40:44 +0200

    Game running

--- src/game/Game.java
+++ src/game/Game.java
@@ -5,6 +5,9 @@ public class Game {
     private final GameSpace gameSpace;
     private final GameState gameState;
 
+    //O
+
+
     public Game() {
         this.gameSpace = new GameSpace();
         this.gameState = new GameState();
@@ -12,5 +15,19 @@ public class Game {
 
     public void show() {
         System.out.println(gameSpace);
+    }
+
+    public void run(){
+        while(gameState.isRunning()){
+            //PRINT DE LETAT DU JEU
+            System.out.println(toString());
+            //DEMANDE DE COMMANDE DE MOUVEMENT
+            
+        }
+    }
+
+
+    public String toString(){
+        return gameSpace.toString();
     }
 }
