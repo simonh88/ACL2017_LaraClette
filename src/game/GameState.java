ecc23b32b0d971770a9f2e5764c3ad3b4e780864
simonh88 <simon.hajek2@etu.univ-lorraine.fr>
2017-10-19 11:40:44 +0200

    Game running

--- src/game/GameState.java
+++ src/game/GameState.java
@@ -7,17 +7,23 @@ import characters.Monster;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by simon on 19/10/17.
- */
+
 public class GameState {
     private Hero hero;
     private List<Monster> lMonsters;
 
+    private static enum State {
+        Running,
+        Victory,
+        Loss
+    };
+
+    public State state;
 
     public GameState(){
         lMonsters = new ArrayList<>();
         hero = new Hero(0,0);
+        state = State.Running;
     }
 
 
@@ -31,6 +37,14 @@ public class GameState {
         }
         //ERROR
         return null;
+    }
+
+    public State getState(){
+        return state;
+    }
+
+    public boolean isRunning(){
+        return this.state == State.Running;
     }
 
 
