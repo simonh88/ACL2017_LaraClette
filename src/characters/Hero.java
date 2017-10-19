6b460cbc6d6852e2a26a058b8d60e0540b778c0a
AntoineCourtil <courtil.antoine@hotmail.fr>
2017-10-19 11:17:13 +0200

    mise en place characters

--- src/characters/Hero.java
+++ src/characters/Hero.java
@@ -3,5 +3,25 @@ package characters;
 /**
  * Created by simon on 19/10/17.
  */
-public class Hero implements Character {
+public class Hero extends Character {
+
+    private int hp;
+    private int posX;
+    private int posY;
+
+    public Hero(){
+        this.hp = 10;
+        this.posX = 3;
+        this.posY = 3;
+    }
+
+    public Hero(int x, int y){
+        this.hp = 10;
+        this.posX = x;
+        this.posY = y;
+    }
+
+    public String toString() {
+        return "H";
+    }
 }
