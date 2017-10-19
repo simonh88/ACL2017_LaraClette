ac0eb01835745b19c65b734af7d6d4fb58d6916d
simonh88 <simon.hajek2@etu.univ-lorraine.fr>
2017-10-19 11:14:59 +0200

    Mise en place GameSpace

--- src/game/Game.java
+++ src/game/Game.java
@@ -3,8 +3,10 @@ package game;
 public class Game {
 
     private final GameSpace gameSpace;
+    private final GameState gameState;
 
     public Game() {
         this.gameSpace = new GameSpace();
+        this.gameState = new GameState();
     }
 }
