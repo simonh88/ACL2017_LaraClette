1532dc14ed893551912696a813952887bd4146f7
Guillaume Petit <guillaume.petit4@etu.univ-lorraine.fr>
2017-10-19 11:06:26 +0200

    Ajout des murs à l'affichage sans les classes Decor

--- src/environement/Room.java
+++ src/environement/Room.java
@@ -6,7 +6,7 @@ public class Room {
 
     public Room() {
         room = new char[12][12];
-        setupBorderWall(room);
+        setupRoomAndBorder(room);
     }
 
     public String toString() {
@@ -18,18 +18,55 @@ public class Room {
         for (int j = 0; j < room.length; j++) {
             sb.append("|");
             for (int i = 0; i < room[0].length; i++) {
-                sb.append("__|");
+
+
+
+
+                if (room[j][i] == ' ') {
+                    sb.append("__|");
+                }
+
+                if (room[j][i] == 'W') {
+                    sb.append("WW|");
+                }
+
             }
             sb.append("\n");
         }
         return sb.toString();
     }
 
-    private void setupBorderWall(Room room) {
-        for (int j = 0; j < room.room.length; j++) {
-            for (int i = 0; i < room.room[0].length; i++) {
+    /**
+     * Place les murs sur le bord de la room
+     * @param room
+     */
+    private void setupRoomAndBorder(char[][] room) {
 
+        // Le vide central
+        for (int j = 0; j < room.length; j++) {
+            for (int i = 0; i < room[0].length; i++) {
+                room[j][i] = ' ';
             }
+        }
+
+        // Mur à gauche
+        for (int j = 0; j < room.length; j++) {
+            room[j][0] = 'W';
+        }
+
+        // Mur à droite
+        for (int j = 0; j < room.length; j++) {
+            room[j][room[0].length-1] = 'W';
+        }
+
+        // Mur en haut
+        for (int i = 0; i < room[0].length; i++) {
+            room[0][i] = 'W';
+        }
+
+        // Mur à droite
+        for (int i = 0; i < room.length; i++) {
+            room[room.length - 1][i] = 'W';
         }
     }
 }
