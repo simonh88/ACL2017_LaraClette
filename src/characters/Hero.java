9e6874baae08ec6bcc7d9604b2da27216b68f9ff
AntoineCourtil <courtil.antoine@hotmail.fr>
2017-10-19 12:10:45 +0200

    set PosX Hero

--- src/characters/Hero.java
+++ src/characters/Hero.java
@@ -39,4 +39,16 @@ public class Hero extends Character {
     public String toString() {
         return "H";
     }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
 }
