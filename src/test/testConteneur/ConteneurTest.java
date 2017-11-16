a80dd284d9e15df4fab8e3faa257a16b1b875bfd
Guillaume Petit <guillaume.petit4@etu.univ-lorraine.fr>
2017-11-16 11:29:50 +0100

    Test unitaire TD

--- src/test/testConteneur/ConteneurTest.java
+++ src/test/testConteneur/ConteneurTest.java
@@ -1,23 +1,30 @@
 package test.testConteneur;
 
+import org.junit.jupiter.api.Test;
+
 class ConteneurTest {
 
-    public ConteneurTest() throws ErreurConteneur {
+    @Test
+    void ajouter() throws ErreurConteneur {
         // Right
         Conteneur conteneur1 = new ConteneurImpl(1);
         conteneur1.ajouter(new Object());
 
         // Right
-        Conteneur conteneur1 = new ConteneurImpl(2);
+        conteneur1 = new ConteneurImpl(2);
         conteneur1.ajouter(new Object());
         conteneur1.ajouter(new Object());
-
-        // Boundary
-
-
     }
 
-    public static void main(String[] args) throws ErreurConteneur {
-        new ConteneurTest();
+    @Test
+    void retirer() throws ErreurConteneur {
+        Conteneur c = new ConteneurImpl(4) ;
+
+        Object o = new Object();
+        c.ajouter(o);
+        c.ajouter(new Object());
+        c.retirer(o);
+        c.ajouter(new Object());
+        c.ajouter(new Object());
     }
 }
