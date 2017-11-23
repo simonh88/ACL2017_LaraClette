02936c746c15b3b88c7c35df0613fe3e90353604
Guillaume Petit <guillaume.petit4@etu.univ-lorraine.fr>
2017-11-23 09:25:06 +0100

    Refactoring des test de conteneurImpl (TP/TD

--- src/test/testConteneur/ConteneurTest.java
+++ src/test/testConteneur/ConteneurTest.java
@@ -1,98 +1,89 @@
 package test.testConteneur;
 
 import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
+
 import static org.junit.jupiter.api.Assertions.fail;
 
 class ConteneurTest {
 
+    // ===== Ajouter ===== //
+
     @Test
-    void ajouter() throws ErreurConteneur {
+    void rightAjouterOne() throws ErreurConteneur {
         // Right
         Conteneur conteneur1 = new ConteneurImpl(1);
         conteneur1.ajouter(new Object());
+    }
 
+    @Test
+    void rightAjouterTwo() throws ErreurConteneur {
         // Right
-        conteneur1 = new ConteneurImpl(2);
+        Conteneur conteneur1 = new ConteneurImpl(2);
         conteneur1.ajouter(new Object());
         conteneur1.ajouter(new Object());
+    }
 
-        // Boundary
+    @Test
+    void boundaryAjouterNull() throws ErreurConteneur {
         Conteneur conteneur3 = new ConteneurImpl(1);
         conteneur3.ajouter(null);
+    }
 
-        // Boundary
+    @Test
+    void boundaryAjouterSameObject() throws ErreurConteneur {
+        Conteneur conteneur4 = new ConteneurImpl(2);
+        Object o1 = new Object();
+        conteneur4.ajouter(o1);
         try {
-            Conteneur conteneur4 = new ConteneurImpl(2);
-            Object o1 = new Object();
             conteneur4.ajouter(o1);
-            conteneur4.ajouter(o1);
-        }catch (ErreurConteneur erreurConteneur){
-            System.out.println(erreurConteneur);
+            fail("L'objet a été ajouté deux fois");
+        } catch (ErreurConteneur e) {
+            // Cette exception est normale
         }
-
-        // Boundary
-        try {
-            Conteneur conteneur4 = new ConteneurImpl(2);
-            Object o1 = new String();
-            o1 = "coucou";
-            conteneur4.ajouter(o1);
-            conteneur4.ajouter(o1);
-        }catch (ErreurConteneur erreurConteneur){
-            System.out.println(erreurConteneur);
-        }
-
-        // Boundary
-        try {
-            Conteneur conteneur4 = new ConteneurImpl(2);
-
-            conteneur4.ajouter(new Object());
-            conteneur4.ajouter(new Object());
-        }catch (ErreurConteneur erreurConteneur){
-            System.out.println(erreurConteneur);
-        }
-
-        // Boundary
-        try {
-            Conteneur conteneur4 = new ConteneurImpl(1);
-
-            conteneur4.ajouter(new Object());
-            conteneur4.ajouter(new Object());
-        }catch (Exception erreurConteneur){
-            System.out.println(erreurConteneur);
-        }
-
-        // Crosscheck
-        try {
-            Conteneur conteneur4 = new ConteneurImpl(4);
-
-            conteneur4.ajouter(new Object());
-            conteneur4.ajouter(new Object());
-            conteneur4.ajouter(new Object());
-            conteneur4.ajouter(new Object());
-        }catch (Exception erreurConteneur){
-            System.out.println(erreurConteneur);
-        }
-
-        try {
-            HashSet<Object> conteneur4 = new HashSet<>();
-
-            conteneur4.add(new Object());
-            conteneur4.add(new Object());
-            conteneur4.add(new Object());
-            conteneur4.add(new Object());
-        }catch (Exception erreurConteneur){
-            System.out.println(erreurConteneur);
-        }
-
 
     }
 
     @Test
-    void retirer() throws ErreurConteneur {
-        Conteneur c = new ConteneurImpl(4) ;
+    void boundaryAjouterSameString() throws ErreurConteneur {
+        Conteneur conteneur4 = new ConteneurImpl(2);
+        Object o1 = new String();
+        o1 = "coucou";
+        conteneur4.ajouter(o1);
+        try {
+            conteneur4.ajouter(o1);
+            fail("La chaine de char a été ajouté deux fois");
+        } catch (ErreurConteneur e) {}
+
+    }
+
+    /**
+     * Ajoute deux éléments avec une capacité de 1
+     *
+     * @throws ErreurConteneur
+     */
+    @Test
+    void boundaryAjouterTwoFailCap() throws ErreurConteneur {
+        Conteneur conteneur4 = new ConteneurImpl(1);
+        conteneur4.ajouter(new Object());
+        try {
+            conteneur4.ajouter(new Object());
+            fail("On a ajouté deux éléments avec une capacité à 1");
+        } catch (ErreurConteneur e) {
+            // Cette exception est normale
+        }
+
+    }
+
+
+    // ===== Retirer ===== //
+
+    @Test
+    void rightRetirerOne() throws ErreurConteneur {
+        Conteneur c = new ConteneurImpl(4);
 
         Object o = new Object();
         c.ajouter(o);
@@ -100,18 +91,25 @@ class ConteneurTest {
         c.retirer(o);
         c.ajouter(new Object());
         c.ajouter(new Object());
+    }
 
-
-
+    @Test
+    void rightRetirerNonPresent() throws ErreurConteneur {
+        Conteneur c;
         c = new ConteneurImpl(4);
-        o = new Object();
+        Object o = new Object();
         try {
             c.retirer(o);
             fail("On a retiré un objet non présent");
         } catch (ErreurConteneur ec) {
-
+            // Cette exception est normale
         }
+    }
 
+    @Test
+    void rightRetirerDeuxFois() throws ErreurConteneur {
+        Conteneur c;
+        Object o;
 
         c = new ConteneurImpl(5);
         o = new Object();
@@ -123,38 +121,41 @@ class ConteneurTest {
             c.retirer(p);
             fail("L'objet P a été retiré deux fois");
         } catch (ErreurConteneur ec) {
-
+            // Cette exception est normale
         }
-
-
-
     }
 
+    // ===== Redimensioner ===== //
+
     @Test
-    void redimensioner(){
-        // Fail
+    void rightRedimensionerMoinsUn() throws ErreurConteneur {
         Conteneur c1 = new ConteneurImpl(1);
         try {
             c1.redimensionner(-1);
-        } catch (Exception e){
-            System.out.println(e);
+            fail("Redimensionement 1 -> -1");
+        } catch (Exception e) {
+            // Cette exception est normale
         }
+    }
 
-        // Right
+    @Test
+    void rightRedimensionerDouble() throws ErreurConteneur {
         Conteneur c2 = new ConteneurImpl(1);
-        try {
-            c2.redimensionner(2);
-        } catch (Exception e){
-            System.out.println(e);
-        }
+        c2.redimensionner(2);
+    }
 
-        // Fail
+    @Test
+    void rightRedimensionerZero() throws ErreurConteneur {
         Conteneur c3 = new ConteneurImpl(1);
         try {
             c3.redimensionner(0);
-        } catch (Exception e){
-            System.out.println(e);
+            fail("Redimensionement à 1 -> 0");
+        } catch (Exception e) {
+            // Cette exception est normale
         }
-
     }
+
+
+
+
 }
