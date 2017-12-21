beaff9a2602fe1f6bba671843c8ace54922a2c0b
GZablot <zablot.guillaume@bbox.fr>
2017-12-21 15:24:31 +0100

    Maj SoundFactory

--- src/factory/SoundFactory.java
+++ src/factory/SoundFactory.java
@@ -19,6 +19,18 @@ public class SoundFactory {
 
     private SoundFactory(){
 
+        // background
+        try {
+            AudioInputStream audioInputStream =
+                    AudioSystem.getAudioInputStream(new File("res/sound/Background_Retro.wav"));
+            background = AudioSystem.getClip();
+            background.open(audioInputStream);
+            background.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (Exception ex){
+            System.out.println("Impossible de charger res/sound/Background_Retro.wav");
+            System.exit(-1);
+        }
+
         // attack hero
         try {
             AudioInputStream audioInputStream =
@@ -103,22 +115,6 @@ public class SoundFactory {
         return instance;
     }
 
-    public void playSound(String fichier){
-        // fonctionne qu'avec .wav
-        try {
-
-
-            AudioInputStream audioInputStream =
-                    AudioSystem.getAudioInputStream(new File(fichier));
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioInputStream);
-            clip.start();
-        }catch (Exception ex){
-            System.out.println("Impossible de charger " + fichier);
-            //System.exit(-1);
-        }
-    }
-
 
     public void playAttackSword(){
         attackSword.setFramePosition(0);
@@ -150,18 +146,7 @@ public class SoundFactory {
     }
 
     public void playBackground(){
-        try {
-            AudioInputStream audioInputStream =
-                    AudioSystem.getAudioInputStream(new File("res/sound/Background_Retro.wav"));
-            background = AudioSystem.getClip();
-            background.open(audioInputStream);
-            background.loop(Clip.LOOP_CONTINUOUSLY);
-            background.start();
-        }catch (Exception ex){
-            ex.printStackTrace();
-            System.out.println("Impossible de charger res/sound/Background_Retro.wav");
-            System.exit(-1);
-        }
+        background.start();
     }
 
     public void stopBackground(){
